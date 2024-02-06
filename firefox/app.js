fc23b4e2e2a8c8a7cc452b5f8864f454f0246993
Adrian-Oliveira <adrianalves483@gmail.com>
2024-02-05 22:30:50 -0300

    Board centralized, deal with sidebar and updating statistics width
    Board centralized with pixel perfect, extension dealing this the
    change in the sidebar width and statistics-sidebar with width update

--- firefox/app.js
+++ firefox/app.js
@@ -1,10 +1,51 @@
+//troubleshooting
+
+// Create a new div element
+const line = document.createElement('div');
+
+// Style the div to represent the vertical red line
+line.style.position = 'fixed';
+line.style.top = '0';
+line.style.bottom = '0';
+line.style.left = '50%'; // Position in the middle horizontally
+line.style.width = '2px'; // Width of the line
+line.style.backgroundColor = 'red'; // Color of the line
+
+// Append the div to the body of the document
+document.body.appendChild(line);
+
+
+
+
+
 const body = document.body
 const board = document.getElementById("board-layout-main")
 const sideBar = document.getElementById("sb")
 
-const screenWidth = body.offsetWidth
-const boardWidth = board.offsetWidth
-const sideBardWidth = sideBar.offsetWidth
+const computedStyle = window.getComputedStyle(body)
 
-board.style.paddingLeft = `${((screenWidth - boardWidth )/2) - sideBardWidth}px`
+const screenWidth = body.offsetWidth
+let bodyPaddingLeft =  parseFloat(computedStyle.getPropertyValue('padding-left'));
+let boardWidth = board.offsetWidth
+
+const centralize = ()=>{
+    bodyPaddingLeft =  parseFloat(computedStyle.getPropertyValue('padding-left'))
+    board.style.paddingLeft = `${((screenWidth - boardWidth )/2) - bodyPaddingLeft}px`
+}
+centralize();
+
+const resizeObserver = new ResizeObserver(() => {
+    centralize();
+});
+
+resizeObserver.observe(sideBar);
+
+
+
+// fixing sidebar-component width
+const liveStats = document.querySelector(".live-stats-component")
+const sideBarComponent = document.querySelector(".sidebar-component")
+
+sideBarComponent.style.width = `${sideBarComponent.offsetWidth +25}px`
+
 
